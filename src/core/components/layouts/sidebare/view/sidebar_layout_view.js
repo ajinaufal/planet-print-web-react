98b3707b6a73659912fa8e31f16ed91f45483d0b
Aji Naufal Aqil <aji.aqil_aam@AM-085404.HOSDG>
2024-05-17 18:14:08 +0700

    fix: feat sidebar refactor

--- src/core/components/layouts/sidebare/view/sidebar_layout_view.js
+++ src/core/components/layouts/sidebare/view/sidebar_layout_view.js
@@ -4,18 +4,12 @@ import { ImageConstants } from '../../../../utils/constants/image_constant';
 
 export function SidebarLayout() {
   return (
-    <aside className="flex flex-col gap-3 bg-target-primary-hover h-screen w-fit min-w-52 pt-8">
+    <aside className="flex flex-col gap-3 bg-target-primary-hover h-screen w-fit min-w-44 pt-8">
       <img src={ImageConstants.beautyProfile} className="w-36 px-4 mx-auto" />
       {(SidebarMenus || []).map((menu, index) => (
         <Link
           id={index}
-          className="text-white hover:bg-white hover:text-black rounded-l-lg px-4 py-2"
-          // style={{
-          //   WebkitMaskImage:
-          //     'radial-gradient(circle 10px at 0 0, transparent 0, transparent 20px, black 21px)',
-          //   maskImage:
-          //     'radial-gradient(circle 10px at 0 0, transparent 0, transparent 20px, black 21px)',
-          // }}
+          className="text-white hover:bg-white hover:text-black rounded-l-lg ml-4 pl-4 pr-6 py-2"
         >
           {menu.name}
         </Link>
