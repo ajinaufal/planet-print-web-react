d9213357493d1e93b831b9a4d2d30ec6f9c46635
Aji Naufal Aqil <ajinaufalaqil@Ajis-MacBook-Pro.local>
2024-05-24 07:21:27 +0700

    refactor: fix create users

--- src/core/router/routers.js
+++ src/core/router/routers.js
@@ -7,29 +7,31 @@ import LayoutView from '../../core/components/layouts/main/view/layout_view';
 import ListProductView from '../../features/product/list_product/view/list_product_view';
 import CreateProductView from '../../features/product/create_product/view/create_product_view';
 import ListCategoryView from '../../features/category/list_category/view/list_category_view';
+import ListUserView from '../../features/user/list_user/view/list_user_view';
 
 export function Router(data) {
-  return createBrowserRouter([
-    {
-      path: '/',
-      element: <LayoutView {...data} />,
-      children: [...routersNonAuth(data), ...routersAuth(data)],
-    },
-  ]);
+    return createBrowserRouter([
+        {
+            path: '/',
+            element: <LayoutView {...data} />,
+            children: [...routersNonAuth(data), ...routersAuth(data)],
+        },
+    ]);
 }
 
 export function routersNonAuth(data) {
-  return [
-    { path: 'login', element: <LoginView {...data} /> },
-    { path: 'register', element: <RegisterView {...data} /> },
-  ];
+    return [
+        { path: 'login', element: <LoginView {...data} /> },
+        { path: 'register', element: <RegisterView {...data} /> },
+    ];
 }
 
 export function routersAuth(data) {
-  return [
-    // { index: true, element: <DashboardView {...data} /> },
-    { path: 'product', element: <ListProductView {...data} /> },
-    { path: 'product/create', element: <CreateProductView {...data} /> },
-    { path: 'category', element: <ListCategoryView {...data} /> },
-  ];
+    return [
+        // { index: true, element: <DashboardView {...data} /> },
+        { path: 'product', element: <ListProductView {...data} /> },
+        { path: 'product/create', element: <CreateProductView {...data} /> },
+        { path: 'category', element: <ListCategoryView {...data} /> },
+        { path: 'user', element: <ListUserView {...data} /> },
+    ];
 }
--- src/core/utils/constants/menu_sidebar_constants.js
+++ src/core/utils/constants/menu_sidebar_constants.js
@@ -1,8 +1,8 @@
 export const SidebarMenus = [
-  { name: 'Dashboard', link: '/', children: [] },
-  { name: 'Orders', link: '/', children: [] },
-  { name: 'Product', link: '/product', children: [] },
-  { name: 'Category', link: '/category', children: [] },
-  { name: 'Users', link: '/', children: [] },
-  { name: 'Config', link: '/', children: [] },
+    { name: 'Dashboard', link: '/', children: [] },
+    { name: 'Orders', link: '/', children: [] },
+    { name: 'Product', link: '/product', children: [] },
+    { name: 'Category', link: '/category', children: [] },
+    { name: 'Users', link: '/user', children: [] },
+    { name: 'Config', link: '/', children: [] },
 ];
